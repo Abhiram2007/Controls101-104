a0b67ed2478b0f6a01cc4cada51b3d35f9a257c0
Abhiram2007 <mandaabhiram234@gmail.com>
2022-09-15 20:06:44 -0700

    added useless stuff

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Car blah = new Car("Bugatti", 200);
-        Sedan foo = new Sedan("jeff", 113);
+        Sedan foo = new Sedan("jeff", 113, true);
         System.out.println(blah.getModelName() + " " +  blah.getTopSpeed());
         blah.setTopSpeed(250);
         blah.setModelName("honda");
--- src/Sedan.java
+++ src/Sedan.java
@@ -1,9 +1,20 @@
-public class Sedan extends Car{
-    public Sedan(String modelName, int topSpeed){
+public class Sedan extends Car implements Driveable{
+    private int numWheels;
+    private boolean gotInACarAccident;
+    public Sedan(String modelName, int topSpeed, boolean gotInACarAccident){
         super(modelName, topSpeed);
+        this.gotInACarAccident = gotInACarAccident;
+        numWheels = 4;
     }
     @Override
     public String getSound(){
         return "vroom but sedan";
     }
+    public int numWheels(){
+        if(gotInACarAccident){
+            return numWheels - 4;
+        }
+        else return numWheels;
+    }
 }
+
